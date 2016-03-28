cfdb69d5c201ce2497efdd4e10e7b398a076c2e4
Wilson Rondini <wilbecoding@gmail.com>
2016-03-27 18:43:36 -0600

    Sized Background Image

---
+++ public/javascripts/jQuery.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  var height = $(".header").height();
+  var width = $(".header").width();
+
+  console.log('  height   ' + height);
+  console.log('  width    ' + width);
+
+});
