03167f23c8d18c8fe4521297cc486db42ddf809c
Wilson Rondini <wilbecoding@gmail.com>
2016-04-02 14:48:53 -0600

    Header transition scaffolding in place

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -9,10 +9,17 @@ $(document).ready(function() {
   $(window).load(function() {
     $(window).scroll(function() {
     if ($(this).scrollTop() > 1){  
+        $(".testUnderImg > h1").remove();
+        $(".testUnderImg1 > ul").remove();
         $('.header').addClass("sticky");
+        $('.circleImage').addClass('circleImageHeader')
       }
       else{
         $('.header').removeClass("sticky");
+        $('.circleImage').removeClass('circleImageHeader')
+        $(".testUnderImg > h1").text("Wilson Rondini");
+        $(".testUnderImg1 > ul").remove();
+
       }
     });
   });
