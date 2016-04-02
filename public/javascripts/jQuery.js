53cf83011e622bf1f6f8d3ccf60b073692328cfa
Wilson Rondini <wilbecoding@gmail.com>
2016-04-02 14:23:28 -0600

    Header animation git branch test

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -1,8 +1,20 @@
+
 $(document).ready(function() {
   var height = $(".header").height();
   var width = $(".header").width();
 
-  console.log('  height   ' + height);
-  console.log('  width    ' + width);
-
+  console.log("  height   " + height);
+  console.log("  width    " + width);
+  
+  $(window).load(function() {
+    $(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $('.header').addClass("sticky");
+      }
+      else{
+        $('.header').removeClass("sticky");
+      }
+    });
+  });
 });
+
