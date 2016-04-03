e2c26c957396a6a34b88ff224aebb84f888c6669
Wilson Rondini <wilbecoding@gmail.com>
2016-04-02 20:49:42 -0600

    OnHover Spotify Scaffolding is in place

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -2,19 +2,35 @@ $(document).ready(function() {
   var height = $(".header").height();
   var width = $(".header").width();
 
-  console.log('  height   ' + height);
-  console.log('  width    ' + width);
+  // console.log('  height   ' + height);
+  // console.log('  width    ' + width);
 
   $("#spotify").hover( function(){
-    $("#spotify").animate({height: 700},
-     "slow",
-     "swing");
+    console.log("Triggers")
+    $("#spotify").animate({
+      height: 540, 
+    }, 1000,
+     function(){
+      $('.followBttn').removeClass("hidden");
+      $(this).unbind('mouseenter mouseleave')
+     })
+    return false
+     // function(){
+     //  position:fixed;
+     // });
     // $('#spotify').height(700);
     // $('#spotify').slideDown('slow');
   });
 
   $("#spotify").mouseout( function(){
-    $("#spotify").animate({height: 98}, "slow");
+    $("#spotify").animate({
+      height: 98
+    }, 1000,
+     function(){
+      $('.followBttn').addClass("hidden");
+      $(this).bind('mouseenter mouseleave')
+      // position: static;
+     });
  });
 
 });
