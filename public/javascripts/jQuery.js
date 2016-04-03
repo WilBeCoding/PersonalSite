67dc254f5cb92810b4ca549a7f661a0cb9739962
Wilson Rondini <wilbecoding@gmail.com>
2016-04-02 19:01:04 -0600

    Animation in place. Flashes. Bugged out

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
   console.log('  height   ' + height);
   console.log('  width    ' + width);
 
+  $("#spotify").hover( function(){
+    $("#spotify").animate({height: 700},
+     "slow",
+     "swing");
+    // $('#spotify').height(700);
+    // $('#spotify').slideDown('slow');
+  });
+
+  $("#spotify").mouseout( function(){
+    $("#spotify").animate({height: 98}, "slow");
+ });
+
 });
