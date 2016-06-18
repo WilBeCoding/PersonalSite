300b897b58220f17c0c2393bf0a6a1e5bfcefd64
Wilson Rondini <wilbecoding@gmail.com>
2016-06-18 15:30:22 -0600

    Header Transition Tweaks. Now needs styling and animations

--- public/javascripts/jQuery.js
+++ public/javascripts/jQuery.js
@@ -6,25 +6,40 @@ $(document).ready(function() {
   console.log("  height   " + height);
   console.log("  width    " + width);
   
-  $(window).load(function() {
+  // $(window).load(function() {
     $(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $(".testUnderImg").remove();
-        $(".testUnderImg1").remove();
-        $(".header").addClass("sticky");
-        $(".circleImage").addClass("circleImageHeader");
-        $('.spotify').css('visibility','visible').hide().slideDown();
-        $('.spotify').css('display','flex');
-        $('.socialMedia').css('visibility','visible').hide().slideDown();
-        $('.socialMedia').css('display','flex');
-        // $('.followBttn').css('visibility','visible').hide().slideDown();
-        // $('.followBttn').css('display', 'flex');
-      }
-      else{
-        $(".header").removeClass("sticky");
-        $(".circleImage").removeClass("circleImageHeader");
-      }
-    });
-  });
+      if ($(this).scrollTop() > 1){  
+          $(".testUnderImg").css('visibility','hidden').hide().slideUp();
+          $(".testUnderImg1").css('visibility','hidden').hide().slideUp();
+          $(".header").addClass("sticky");
+          $(".circleImage").addClass("circleImageHeader");
+          $(".circleImageHeader").removeClass("circleImage");
+          $('.spotifyHide').addClass('spotify');
+          $('.spotify').removeClass('spotifyHide')
+          $('.spotify').css('display','flex');
+          $('.headerName').css('visibility','visible').hide().slideDown();
+          $('.headerEmail').css('display','flex');
+          $('.headerEmail').css('visibility','visible');
+          $('.socialMedia').css('display','flex');
+          // $('.followBttn').css('visibility','visible').hide().slideDown();
+          // $('.followBttn').css('display', 'flex');
+      } else if($(this).scrollTop() <= 1) {
+          $(".testUnderImg").css('visibility','visible');
+          $(".testUnderImg1").css('visibility','visible');
+          $(".testUnderImg").css('display','')
+          $(".testUnderImg1").css('display','')
+          $(".header").removeClass("sticky");
+          $(".circleImageHeader").addClass("circleImage");
+          $(".circleImage").removeClass("circleImageHeader");
+          $('.spotify').addClass('spotifyHide');
+          $('.spotifyHide').removeClass('spotify')
+          $('.spotifyHide').css('display','none').hide();
+          $('.headerName').css('visibility','hidden');
+          $('.headerEmail').css('visibility','hidden');
+          $('.socialMedia').css('display','none');
+          $('.headerEmail')
+
+        };
+      });
 });
 
